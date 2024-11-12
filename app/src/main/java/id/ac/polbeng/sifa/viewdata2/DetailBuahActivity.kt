0360c124cb9ba82e6a21d25970ce345a5b30828a
sifa <sf7687138@gmail.com>
2024-11-12 12:59:53 +0700

    Menampilkan Data Menggunakan Listview dan Recyclerview

---
+++ app/src/main/java/id/ac/polbeng/sifa/viewdata2/DetailBuahActivity.kt
@@ -0,0 +1,25 @@
+package id.ac.polbeng.sifa.viewdata2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.sifa.viewdata2.databinding.ActivityDetailBuahBinding
+
+
+class DetailBuahActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBuahBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBuahBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //membuat variabel dan di tangkap data dri intent
+        var namaBuah = intent.getStringExtra("nama")
+        var image = intent.getIntExtra("gambar",0)
+
+        //menangkap data intent dan di set ke layout
+        binding.tvNamaBuah.text = namaBuah
+        binding.imgBuah.setImageResource(image)
+    }
+}
